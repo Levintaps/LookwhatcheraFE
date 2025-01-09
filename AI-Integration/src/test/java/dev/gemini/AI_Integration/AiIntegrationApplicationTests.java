5c96c47ae2719b308997cbcf565d6369ce797940
Levintaps <135119762+Levintaps@users.noreply.github.com>
2025-01-10 00:17:45 +0800

    First Commit

---
+++ AI-Integration/src/test/java/dev/gemini/AI_Integration/AiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gemini.AI_Integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
